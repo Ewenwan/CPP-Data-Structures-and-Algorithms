4c2ef2f94ecc52cf371b0d51b77c215b88b6195a
Wan Youwen <1756429307@qq.com>
2019-01-14 15:05:10 +0800

    Update main.cpp

--- Chapter01/Constructor/main.cpp
+++ Chapter01/Constructor/main.cpp
@@ -1,4 +1,4 @@
-// Constructor.cbp
+// 11. 类构造函数实现
 #include <iostream>
 
 using namespace std;
@@ -9,11 +9,13 @@ private:
     string m_name;
 
 public:
+    // 类构造函数实现
     Animal(string name) : m_name(name)
-    {
-
+    { // 使用传入变量 name 为 类内私有变量 m_name 赋值
+      // 创建类实例式可传入对象的名字，省去了 命名函数
     }
-
+    
+    // 还是需要 通过公开的 方法 在外部 访问 类内变量
     string GetName()
     {
         return m_name;
@@ -22,8 +24,9 @@ public:
 
 int main()
 {
+    // 创建类实例式可传入对象的名字，省去了 命名函数
     Animal dog = Animal("dog");
-
+    // 还是需要 通过公开的 方法 在外部 访问 类内变量
     cout << "Hi, I'm a " << dog.GetName() << endl;
 
     return 0;
