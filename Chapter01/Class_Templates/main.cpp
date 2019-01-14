6003967a5231f93204b9248e6abf3ecc86eef492
Wan Youwen <1756429307@qq.com>
2019-01-14 21:09:51 +0800

    15. 类模板 Class templates  类模板=内置类型可以传入类类型=

--- Chapter01/Class_Templates/main.cpp
+++ Chapter01/Class_Templates/main.cpp
@@ -1,4 +1,4 @@
-// Class_Templates.cbp
+// 15. 类模板 Class templates  类模板=内置类型可以传入类类型=
 #include <iostream>
 
 using namespace std;
@@ -13,9 +13,7 @@ public:
     {
 
     }
-
-    // The interface that has to be implemented
-    // in derived class
+    // 虚函数 相当于先占坑，先占茅坑======
     virtual string MakeSound() = 0;
 
     string GetName()
@@ -64,7 +62,7 @@ public:
     }
 
 };
-
+// 函数模板，接收一个类对象============
 template<typename T>
 void GetNameAndMakeSound(T& theAnimal)
 {
@@ -72,15 +70,17 @@ void GetNameAndMakeSound(T& theAnimal)
     cout << theAnimal.MakeSound() << endl;
 }
 
-template <typename T>
-class AnimalTemplate
+// 类模板====内置类型 可以传入 类类型=================
+template <typename T> // 类内 变量的类型 为 模板
+class AnimalTemplate  // 类 名字
 {
 private:
-    T m_animal;
+    T m_animal; // 模板类型 变量=== 可以直接传入类类型===
 
 public:
     AnimalTemplate(T animal) : m_animal(animal) {}
-
+    
+    // 打印 信息
     void GetNameAndMakeSound()
     {
         cout << m_animal.GetName() << " goes ";
@@ -90,10 +90,13 @@ public:
 
 int main()
 {
+    // 类实例 Dog
     Dog dog = Dog("Bulldog");
+    // 传入 Dog类对象实例 构造 模板类实例====
     AnimalTemplate<Dog> dogTemplate(dog);
     dogTemplate.GetNameAndMakeSound();
-
+    
+    // 
     Cat cat = Cat("Persian Cat");
     AnimalTemplate<Cat> catTemplate(cat);
     catTemplate.GetNameAndMakeSound();
