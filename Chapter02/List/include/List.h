7b83b39089a6a8b5b64288af5d26392f619317cc
Wan Youwen <1756429307@qq.com>
2019-01-15 21:45:51 +0800

    列表

--- Chapter02/List/include/List.h
+++ Chapter02/List/include/List.h
@@ -1,5 +1,5 @@
 // Project: List.cbp
-// File   : List.h
+// File   : List.h  列表=====
 #ifndef LIST_H
 #define LIST_H
 
@@ -14,10 +14,15 @@ class List
     public:
         List();
         ~List();
+        // 获取指定索引处的值
         int Get(int index);
+        // 在 指定索引 位置 插入元素
         void Insert(int index, int val);
+        // 在列表 中搜索指定元素 val
         int Search(int val);
+        // 删除指定索引处的 元素
         void Remove(int index);
+        // 统 计 列表  元素数量 
         int Count();
 };
 #endif // LIST_H
