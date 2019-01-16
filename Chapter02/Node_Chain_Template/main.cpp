a78ed33a5366ac23850057433a48b016e582d543
Wan Youwen <1756429307@qq.com>
2019-01-16 10:46:05 +0800

    节点链 模板Template 实现，可存储多种类型的数据

--- Chapter02/Node_Chain_Template/main.cpp
+++ Chapter02/Node_Chain_Template/main.cpp
@@ -1,31 +1,31 @@
-// Project: Node_Chain_Template.cbp
+// 节点链 人物关系链 模板Template 实现，可存储多种类型的数据
 // File   : main.cpp
 #include <iostream>
 
 using namespace std;
 
-template <typename T>
+template <typename T> // template <typename *> 模板数据类型，任意数据类型
 class Node
 {
 public:
-    T Value;
-    Node<T> * Next;
-
-    Node(T value) : Value(value){}
+    T Value; // 模板类型数据
+    Node<T> * Next; // 下一个节点 关系人物 模板类型节点
+    
+    // 类构造函数======初始化两个内部数据===
+    Node(T value) : Value(value), Next(NULL) {}
 };
 
+// 打印函数也是用 模板类型=====
 template<typename T>
-void PrintNode(Node<T> * node)
+void PrintNode(Node<T> * node)// 传入节点指针
 {
-    // It will print the initial node
-    // until it finds NULL for the Next pointer
-    // that indicate the end of the Node Chain
+    // NULL 表明 线索到头了，幕后大佬后面没老虎了===
     while(node != NULL)
     {
-        cout << node->Value << " -> ";
-        node = node->Next;
+        cout << node->Value << " -> ";// 打印当前节点(嫌疑人)的信息(名字，脏污，财产)
+        node = node->Next;            // 找到下一个嫌疑人=====
     }
-
+    // 打印最后的 终止标志 NULL
     cout << "NULL" << endl;
 }
 
@@ -34,16 +34,19 @@ int main()
     // +------+------+
     // | 4.93 | NULL |
     // +------+------+
+    // 节点1 浮点类型数据
     Node<float> * node1 = new Node<float>(4.93);
 
     // +------+------+
     // | 6.45 | NULL |
     // +------+------+
+    // 节点2
     Node<float> * node2 = new Node<float>(6.45);
 
     // +------+------+
     // | 8.17 | NULL |
     // +------+------+
+    // 节点3
     Node<float> * node3 = new Node<float>(8.17);
 
     // +------+------+  +------+------+  +------+------+
@@ -54,6 +57,7 @@ int main()
     // +------+------+  +------+------+  +------+------+
     // | 4.93 |   +---->| 6.45 |   +---->| 8.17 | NULL |
     // +------+------+  +------+------+  +------+------+
+    // 节点1--->节点2--->节点3--->终止NULL
     node2->Next = node3;
 
     // Print the node
