53eccf7462ec354c5a5f3625640acdc10165be18
Wan Youwen <1756429307@qq.com>
2019-01-17 19:06:21 +0800

    Update Stack.h

--- Chapter03/Stack/include/Stack.h
+++ Chapter03/Stack/include/Stack.h
@@ -5,78 +5,71 @@
 
 #include "../../Node/Node.h"
 
-template <typename T>
+template <typename T> // 模板类型
 class Stack
 {
-private:
-    int m_count;
-    Node<T> * m_top;
-
-public:
-    Stack();
-    bool IsEmpty();
-    T Top();
-    void Push(T val);
-    void Pop();
+private:   // 私有数据
+ int m_count;// 熟练
+ Node<T> * m_top; // 栈顶指针
+ 
+ 
+public: // 公开
+ Stack();       // 类构造函数
+ bool IsEmpty();// 空检查
+ T Top();       // 获取栈顶元素
+ void Push(T val);// 栈顶插入元素
+ void Pop();      // 弹出栈顶元素
 };
 
 template <typename T>
 Stack<T>::Stack() : m_count(0), m_top(NULL) {}
 
+// 空检查===============
 template <typename T>
 bool Stack<T>::IsEmpty()
 {
-    // return TRUE if there are no items
-    // otherwise, return FALSE
-    return m_count <= 0;
+ // return TRUE if there are no items
+ // otherwise, return FALSE
+ return m_count <= 0;
 }
 
+// 获取栈顶元素=======
 template <typename T>
 T Stack<T>::Top()
 {
-    // Just return the value
-    // of m_top node
-    return m_top->Value;
+ // Just return the value of m_top node
+ return m_top->Value;
 }
 
+// 栈顶插入元素===========
 template <typename T>
 void Stack<T>::Push(T val)
 {
-    // Create a new Node
-    Node<T> * node = new Node<T>(val);
-
-    // The Next pointer of this new node
-    // will point to current m_top node
-    node->Next = m_top;
-
-    // The new Node now becomes the m_top node
-    m_top = node;
-
-    // One item is added
-    m_count++;
+ // 创建一个新节点
+ Node<T> * node = new Node<T>(val);
+ // 新节点的 后继 指向 原 栈顶节点
+ node->Next = m_top;
+ // 新节点 重置为 新栈顶节点
+ m_top = node;
+ // 数量++
+ m_count++;
 }
 
+// 弹出栈顶元素===========
 template <typename T>
 void Stack<T>::Pop()
 {
-    // Do nothing if Stack is empty
-    if(IsEmpty())
-        return;
-
-    // Prepare the current m_top
-    // to remove
-    Node<T> * node = m_top;
-
-    // The new m_top node will be
-    // the Next pointer of the current m_top node
-    m_top = m_top->Next;
-
-    // Now it's safe to remove
-    // the first element
-    delete node;
-
-    // One item is removed
-    m_count--;
+ // 空栈直接返回
+ if(IsEmpty())
+      return;
+ // 旧栈顶节点，待删除
+ Node<T> * node = m_top;
+ // 旧栈顶节点的后继 设置为 新栈顶节点
+ m_top = m_top->Next;
+ // 删除原 旧栈顶节点
+ delete node;
+ // 数量--
+ m_count--;
 }
 
 #endif // STACK_H
